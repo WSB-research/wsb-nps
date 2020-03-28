948f6212e88ac10b8b785521d225738c71e52d91
Thomas Shallenberger <twshal@gmail.com>
2020-03-28 01:49:56 -0500

    Added herokuapp.com to allowed hosts

--- api/api/settings.py
+++ api/api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
